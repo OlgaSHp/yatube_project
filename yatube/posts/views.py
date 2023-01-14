84a6277a97675eeb0bc88221438ea203df04dde6
Ольга Шполянская <olgab497@gmail.com>
2023-01-14 23:29:36 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Приветствуем на сайте для блогеров!')
+
+
+def group_posts(request, slug):
+    return HttpResponse('Здесь пока пусто, но скоро будет много интересного!')
+
