2fa7932f9632d854c71d5ce0b84faa77db6cd58f
Ольга Шполянская <olgab497@gmail.com>
2023-01-19 12:25:42 +0300

    Create Group model, register Group model,first migration

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-from django.http import HttpResponse
-
+# Импортируем модель, чтобы обратиться к ней
+from .models import Post
 
 def index(request):
-    return HttpResponse('Приветствуем на сайте для блогеров!')
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
-    return HttpResponse('Здесь пока пусто, но скоро будет много интересного!')
+    template = 'posts/group_list.html'
+    text = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'text': text,
+    }
+    return render(request, template, context)
+    
 
